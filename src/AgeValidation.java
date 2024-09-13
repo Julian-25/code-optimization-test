e4ff0ff41eeccd72f7066b7514dbe3635c7d1e38
Julian-25 <jjpedrazab@ufpso.edu.co>
2024-09-13 13:57:23 -0500

    Refactor complete for quiz

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
 public class AgeValidation {
+    //Eliminacion de numeros magicos mediante el uso de variables con la herramienta final static.
+    final static int AGE_LIMIT = 18;
     public static void main(String[] args) {
-        int age = 18;
+        //Se logró con la herramineta "Scanner" el usuario pueda ingresar su edad por consola
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you age");
+        int age = scanner.nextInt();
         
-        if (age >= 18) {
+        //Eliminacion de uso doble del if con la estructura de control "if else"
+        if (age >= AGE_LIMIT) {
             System.out.println("Access granted");
-        }
-        
-        if (age < 18) {
+        } else if (age < 18) {
             System.out.println("Access denied");
-        }
+        }    
     }
 }
--- src/Store.java
+++ src/Store.java
@@ -1,13 +1,17 @@
 public class Store {
-    final static int price1 = 15;
-    final static int price2 = price1 - 5;
-    final static int price3 = price2 -5;
-    final static int p1 = price1 * 2;
-    final static int p2 = price2 * 3;
-    final static int p3 = price3 * 4;
-    final static int totalSales = p1 + p2 + p3;
+    //ELIMINACION DE VALORES MAGICOS MEDIANTE LA HERRAMIENTA FINAL STATIC.
+    final static int PRICE1 = 15;
+    final static int PRICE2 = 10;
+    final static int PRICE3 = 5;
+    final static int TOTAL1 = PRICE1 * 2;
+    final static int TOTAL2 = PRICE2 * 3;
+    final static int TOTAL3 = PRICE3 * 4;
+    final static int totalSales = PRICE1 + PRICE2 + PRICE3;
+    final static int VENTASSUPERIORES = 50;
     public static void main(String[] args) {
-       if (totalSales > 50) {
+        int []Precios = {TOTAL1,TOTAL2,TOTAL3}
+
+       if (totalSales > VENTASSUPERIORES) {
             System.out.println("Good sales performance");
         } else {
             System.out.println("Low sales performance");
